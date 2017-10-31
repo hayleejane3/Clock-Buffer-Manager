c3ce777a7d72623030561089f814e2673216cdea
sahibgoa <pandori@wisc.edu>
2017-10-31 14:46:32 -0500

    implemented the destructor, allocBuf() and readPage()

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -14,7 +14,7 @@
 #include "exceptions/bad_buffer_exception.h"
 #include "exceptions/hash_not_found_exception.h"
 
-namespace badgerdb { 
+namespace badgerdb {
 
 //----------------------------------------
 // Constructor of the class BufMgr
@@ -24,7 +24,7 @@ BufMgr::BufMgr(std::uint32_t bufs)
 	: numBufs(bufs) {
 	bufDescTable = new BufDesc[bufs];
 
-  for (FrameId i = 0; i < bufs; i++) 
+  for (FrameId i = 0; i < bufs; i++)
   {
   	bufDescTable[i].frameNo = i;
   	bufDescTable[i].valid = false;
@@ -41,6 +41,9 @@ BufMgr::BufMgr(std::uint32_t bufs)
 
 // TODO(sahibgoa)
 BufMgr::~BufMgr() {
+    delete hashTable;
+    delete[] bufPool;
+    delete[] bufDescTable;
 }
 
 // TODO(sreejita)
@@ -49,27 +52,82 @@ void BufMgr::advanceClock()
 }
 
 // TODO(sahibgoa)
-void BufMgr::allocBuf(FrameId & frame) 
-{
+void BufMgr::allocBuf(FrameId & frame) {
+    FrameId freeFrameIdx = numBufs, beginClock = clockHand;
+
+		// Use Clock Algorithm to find a free frame
+		do {
+			// If it's valid check other fields otherwise we have a free frame
+			if (bufDescTable[clockHand].valid) {
+				// If the refbit's set clear it otherwise continue checking other fields
+				if (!bufDescTable[clockHand].refbit) {
+					// Ignore pages which are pinned
+					if (bufDescTable[clockHand].pinCnt == 0) {
+						// If the dirty bit is set, flush the page to disk and then proceed
+						if (bufDescTable[clockHand].dirty) {
+							flushFile(bufDescTable[clockHand].file);
+						}
+
+						// Remove the valid entry from the hash table
+						hashTable->remove(
+							bufDescTable[clockHand].file,
+							bufDescTable[clockHand].pageNo
+						);
+
+						// Clear the fields to prepare frame for new user
+						bufDescTable[clockHand].Clear();
+
+						freeFrameIdx = clockHand;
+					}
+				} else {
+					bufDescTable[clockHand].refbit = false;
+				}
+			} else {
+				freeFrameIdx = clockHand;
+			}
+
+			// Advance the clock
+			advanceClock();
+
+		} while (freeFrameIdx == numBufs && clockHand != beginClock);
+
+		// If we didn't find a frame to evict (all were pinned) then throw exception
+		if (freeFrameIdx == numBufs)
+			throw BufferExceededException();
+
+		// Return frame by reference
+		frame = freeFrameIdx;
 }
 
-// TODO(sahibgoa)	
-void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
+// TODO(sahibgoa)
+void BufMgr::readPage(File* file, const PageId pageNo, Page*& page) {
+    FrameId frame;
+    try {
+        hashTable->lookup(file, pageNo, frame);
+				bufDescTable[frame].refbit = true;
+				bufDescTable[frame].pinCnt++;
+				page = &bufPool[frame];
+    } catch (const HashNotFoundException& e) {
+        allocBuf(frame);
+				file->readPage(pageNo);
+        hashTable->insert(file, pageNo, frame);
+				bufDescTable[frame].Set(file, pageNo);
+				page = &bufPool[frame];
+    }
+}
+
+// TODO(hayleejane)
+void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty)
 {
 }
 
 // TODO(hayleejane)
-void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty) 
-{
-}
-
-// TODO(hayleejane)
-void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
+void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page)
 {
 }
 
 // TODO(sreejita)
-void BufMgr::flushFile(const File* file) 
+void BufMgr::flushFile(const File* file)
 {
 }
 
@@ -78,11 +136,11 @@ void BufMgr::disposePage(File* file, const PageId PageNo)
 {
 }
 
-void BufMgr::printSelf(void) 
+void BufMgr::printSelf(void)
 {
   BufDesc* tmpbuf;
 	int validFrames = 0;
-  
+
   for (std::uint32_t i = 0; i < numBufs; i++)
 	{
   	tmpbuf = &(bufDescTable[i]);
